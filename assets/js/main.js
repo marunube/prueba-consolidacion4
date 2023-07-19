1b946bd7679066e68d631dbb4287ebb1d2a32207
marunube <martinariquelmevilla@gmail.com>
2023-07-18 21:03:06 -0400

    add logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
-import budget from '/assets/js/budget.js';
+const addIncomeBtn = document.querySelector(".ingreso-presupuesto__btn");
+const presupuesto = document.querySelector(".presupuesto");
+const addExpense = document.querySelector(".gasto");
+const addExpenseName = document.querySelector(".nombreGasto");
+const addExpenseBtn =document.querySelector(".ingreso-gasto__btn");
+var incomeData = [];
+var incomeTable = document.querySelector(".table-datos");
 
-new budget("app")
+
+addIncomeBtn.addEventListener('click', () => { 
+    incomeData.push(presupuesto.value)
+    console.log(incomeData);
+
+    // incomeTable.append(presupuesto.value)
+
+        let tr = document.createElement('tr');
+    
+        let td1 = document.createElement('td');
+        td1.textContent = presupuesto.value;
+        tr.appendChild(td1);
+        incomeTable.appendChild(tr);
+});
+
+addExpenseBtn.addEventListener('click', () =>{
+console.log(addExpense.value,addExpenseName.value);}
+);
+
+
 
